d9bb1f1fc5cbdc2c7f7c6f00b208c054aa9f4795
Gabino Barrera <nmr.inc@icloud.com>
2020-12-04 20:10:00 -0600

    Twitter OAuth Strategy

--- config/index.js
+++ config/index.js
@@ -11,6 +11,9 @@ const config = {
   facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   linkedinClientId: process.env.LINKEDIN_CLIENT_ID,
   linkedinClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+  sessionSecret: process.env.SESSION_SECRET,
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET
 };
 
 module.exports = { config: config };
